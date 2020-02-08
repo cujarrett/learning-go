8c16897650e272c8b9af70a3486e1a4cfa11d051
Matt Jarrett <cujarrett@gmail.com>
2020-02-07 23:46:47 -0600

    adds deck logic

--- cards/deck.go
+++ cards/deck.go
@@ -5,6 +5,20 @@ import "fmt"
 // Create a new type of "deck" which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
 // Create a print function
 func (d deck) print() {
 	for index, card := range d {
