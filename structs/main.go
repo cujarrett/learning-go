9b98f6b8668aeb2a11cc1a488b1f2fedbab71615
Matt Jarrett <cujarrett@gmail.com>
2020-02-09 13:41:34 -0600

    Completes structs, pointers, and addresses

---
+++ structs/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	myPerson := person{
+		firstName: "Alex",
+		lastName:  "Anderson",
+		contactInfo: contactInfo{
+			email:   "a@b.com",
+			zipCode: 94000,
+		},
+	}
+	myPerson.print()
+	myPerson.updateName("Frank")
+	myPerson.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
